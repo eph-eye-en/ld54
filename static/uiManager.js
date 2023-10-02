cf7aaada186698fc6985a9fb34de6df9897ba080
eph-eye-en <35633365+eph-eye-en@users.noreply.github.com>
2023-10-02 17:58:47 +0100

    Refactor uiManager.js

--- static/uiManager.js
+++ static/uiManager.js
@@ -44,15 +44,13 @@ class UiManager {
 	createMainMenu() {
 		const stack1 = new Stack(STACK_HORIZ,[0.4,0.6]);
 		const stack2 = new Stack(STACK_VERT,[0.5,0.2,0.2,0.2,0.1]);
-		const stack3 = new Stack(STACK_VERT,[0.1,0.3,0.6]);
 		stack1.children[0] = stack2;
-		stack1.children[1] = stack3;
 
 		var but = this.createTransparentButton("Play",
 			() => this.loadUi(this.levelsMenu));
 		but.fontSize = 60;
-
 		stack2.children[1] = but;
+
 		but = this.createTransparentButton("Controls",
 			() => this.loadUi(this.controlsScreen));
 		but.fontSize = 60;
@@ -61,11 +59,13 @@ class UiManager {
 		but = this.createTransparentButton("About",
 			() => this.loadUi(this.aboutScreen));
 		but.fontSize = 60;
-
 		stack2.children[3] = but;
-		but = this.createTransparentButton("Space Ltd", () => this.loadLevel(2));
-		but.fontSize = 60;
-		stack3.children[1] = but;
+
+		const stack3 = new Stack(STACK_VERT,[0.15, 0.3, 0.55]);
+		stack1.children[1] = stack3;
+		const title = new TextBox("Space LTD", this.theme.text.colour, 90,
+			RIGHT, CENTER);
+		stack3.children[1] = title;
 
 		return stack1;
 	}
@@ -132,14 +132,10 @@ class UiManager {
 		this.ui.root = root;
 	}
 
-	loadLevelsMenu() {
-		this.ui.root = this.levelsMenu;
-	}
-
 	loadLevel(idx) {
 		const succ = this.levelManager.loadLevel(idx);
 		if(succ) {
-			this.ui.root = this.levelScreen;
+			this.loadUi(this.levelScreen);
 			this.levelCompletePopup.hidePopup();
 		}
 	}
